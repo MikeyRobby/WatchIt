3a24d1b35e65c7c75c8ba47bdd859aec9facdec6
MikeyRobby <mikeyrobby81@gmail.com>
2026-04-16 14:45:42 -0700

    testing the watch item struct

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Success")
+	fmt.Println(addItem())
+}
+
+func addItem() Item {
+	watch := Item{Brand: "Seiko", Model: "5 Sport", CaseSize: "38mm"}
+	return watch
 }
